1e786c867f171739a5824d3164d8c2c3c42a8820
Stuart McIntosh <s.g.mcintosh@gmail.com>
2018-02-04 12:58:17 +1030

    adding tests

--- profiler/accounts/tests.py
+++ profiler/accounts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from profiler.views import home
+
+class HomePageTest(TestCase):
+    ''' test of URL and html for home page '''
+    def test_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+   
